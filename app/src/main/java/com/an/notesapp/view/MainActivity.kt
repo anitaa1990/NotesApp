6e94085429071f4c37c1d623cc9b9d20f3a4f843
Anitaa Murthy <murthyanitaa@gmail.com>
2024-11-26 15:44:21 +0530

    Added Empty screen, loading screen + notes UI

--- app/src/main/java/com/an/notesapp/view/MainActivity.kt
+++ app/src/main/java/com/an/notesapp/view/MainActivity.kt
@@ -4,25 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.an.notesapp.Constants.ROUTE_DETAIL_ARG_NAME
+import com.an.notesapp.Constants.ROUTE_DETAIL_PATH
+import com.an.notesapp.Constants.ROUTE_HOME
+import com.an.notesapp.R
 import com.an.notesapp.view.ui.component.AppBarType
 import com.an.notesapp.view.ui.component.MainTopAppBar
+import com.an.notesapp.view.ui.screen.NotesScreen
 import com.an.notesapp.view.ui.theme.NotesAppTheme
+import com.an.notesapp.view.ui.viewmodel.NoteViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import com.an.notesapp.R
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -44,6 +55,8 @@ class MainActivity : ComponentActivity() {
                     derivedStateOf { navController.previousBackStackEntry != null }
                 }
 
+                val viewModel = hiltViewModel<NoteViewModel>()
+
                 Scaffold(
                     topBar = {
                         MainTopAppBar(
@@ -55,7 +68,27 @@ class MainActivity : ComponentActivity() {
                     },
                     modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
                 ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = ROUTE_HOME,
+                        modifier = Modifier.padding(innerPadding).fillMaxSize()
+                    ) {
+                        composable(route = ROUTE_HOME) {
+                            appBarType.value = AppBarType.LARGE
+                            titleId.value = R.string.app_name
 
+                            NotesScreen(viewModel = viewModel) { noteId ->
+                                // TODO
+                            }
+                        }
+                        composable(route = ROUTE_DETAIL_PATH,
+                            arguments = listOf(
+                                navArgument(ROUTE_DETAIL_ARG_NAME) { nullable = true },
+                            )) {
+                            appBarType.value = AppBarType.SMALL
+                            titleId.value = R.string.note_detail_title
+                        }
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/an/notesapp/view/ui/screen/NotesScreen.kt
@@ -0,0 +1,181 @@
+package com.an.notesapp.view.ui.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.an.notesapp.model.db.Note
+import com.an.notesapp.util.getDate
+import com.an.notesapp.util.getTime
+import com.an.notesapp.view.ui.component.EmptyScreen
+import com.an.notesapp.view.ui.component.LoadingItem
+import com.an.notesapp.view.ui.theme.noteTextStyle
+import com.an.notesapp.view.ui.theme.noteTitleStyle
+import com.an.notesapp.view.ui.viewmodel.NoteViewModel
+import com.an.notesapp.R
+import java.time.OffsetDateTime
+
+@Composable
+fun NotesScreen(
+    viewModel: NoteViewModel,
+    onNoteItemClicked: (noteId: Long) -> Unit
+) {
+    val noteUiState = viewModel.notesViewState.collectAsStateWithLifecycle(
+        lifecycleOwner = LocalLifecycleOwner.current
+    )
+
+    if(noteUiState.value.isLoading) {
+        // Show loading
+        LoadingItem()
+
+    } else if (noteUiState.value.notes.isEmpty()) {
+        // show empty screen
+        EmptyScreen()
+
+    } else {
+        // Show notes screen
+        LazyVerticalStaggeredGrid(
+            modifier = Modifier.padding(top = 10.dp, bottom = 10.dp, start = 12.dp, end = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            columns = StaggeredGridCells.Adaptive(minSize = 140.dp),
+        ) {
+            val notes = noteUiState.value.notes
+            items(notes.size) {
+                val note = notes[it]
+                NoteItem(
+                    note = note,
+                    onNoteItemClicked = { onNoteItemClicked(it) },
+                    onNoteItemDeleted = {  }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun NoteItem(
+    note: Note,
+    onNoteItemClicked: (noteId: Long) -> Unit,
+    onNoteItemDeleted: (note: Note) -> Unit
+) {
+    val alpha = if (note.encrypt) 1f else 0f
+    val blur = if (note.encrypt) 10.dp else 0.dp
+    Box(
+        modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
+    ) {
+        Card (
+            modifier = Modifier
+                .padding(bottom = 12.dp)
+                .clickable { onNoteItemClicked(note.id) },
+            shape = RectangleShape,
+            elevation = CardDefaults.cardElevation(10.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            HorizontalDivider (
+                color = MaterialTheme.colorScheme.primary,
+                thickness = 3.dp,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .alpha(alpha),
+            )
+            Column (modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, bottom = 5.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                ) {
+                    Text(
+                        modifier = Modifier.weight(1f),
+                        text = note.title,
+                        style = noteTitleStyle,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer,
+                    )
+                    IconButton(
+                        modifier = Modifier.weight(0.25f),
+                        onClick = { onNoteItemDeleted(note) }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Delete,
+                            contentDescription = "",
+                            tint = MaterialTheme.colorScheme.primary,
+                            modifier = Modifier.size(16.dp)
+                        )
+                    }
+                }
+                Text(
+                    style = noteTextStyle,
+                    modifier = Modifier.padding(end = 10.dp).blur(blur),
+                    text = note.description,
+                    color = MaterialTheme.colorScheme.onPrimaryContainer,
+                    maxLines = 4,
+                    overflow = TextOverflow.Ellipsis
+                )
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                        .padding(top = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = String.format(
+                            stringResource(id = R.string.note_list_date),
+                            note.createdAt.getDate(), note.createdAt.getTime()
+                        ),
+                        modifier = Modifier.weight(1f),
+                        fontSize = 10.sp,
+                        color = MaterialTheme.colorScheme.outline
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Lock,
+                        contentDescription = "",
+                        tint = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier
+                            .size(12.dp)
+                            .weight(0.25f)
+                            .alpha(alpha)
+                    )
+                }
+            }
+        }
+    }
+}
