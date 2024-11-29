a198b5f137883805703524e4e58fd91b34cb4d4d
Anitaa Murthy <murthyanitaa@gmail.com>
2024-11-29 14:07:03 +0530

    Enabled dynamic theme

--- app/src/main/java/com/an/notesapp/view/ui/theme/Theme.kt
+++ app/src/main/java/com/an/notesapp/view/ui/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.an.notesapp.view.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -85,12 +89,18 @@ private val darkScheme = darkColorScheme(
 @Composable
 fun NotesAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-  val colorScheme = when {
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> darkScheme
+        else -> lightScheme
+    }
 
   MaterialTheme(
     colorScheme = colorScheme,
